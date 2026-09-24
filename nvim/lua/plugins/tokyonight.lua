11fe11391504e2b18bbd704ee4de5678acafdac0
ygdstmidn <121283845+ygdstmidn@users.noreply.github.com>
2026-09-25 03:08:07 +0900

    🎨 tokyonightのVeryLazyを削除(なんでこれで動いてたんだ？)

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,7 +2,6 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    event = "VeryLazy",
     opts = {},
     config = function()
         vim.cmd.colorscheme("tokyonight-night")
