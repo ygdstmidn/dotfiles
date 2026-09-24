17e58c89455e55ab114a9bef73288d1cf6574396
ygdstmidn <121283845+ygdstmidn@users.noreply.github.com>
2026-09-24 12:17:19 +0700

    ✨ lualineのfileformatとencodingを基本的に表示しない(特殊な場合のみ表示)

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,6 +4,29 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
-        require("lualine").setup()
+        require("lualine").setup({
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = { "filename" },
+                lualine_x = {
+                    {
+                        -- encoding: エンコーディングがUTF-8の場合は表示しない。
+                        function()
+                            return (vim.bo.fenc or vim.go.enc):gsub("^utf%-8$", "")
+                        end,
+                    },
+                    {
+                        -- fileformat: &ffがunixの場合は表示しない。
+                        function()
+                            return vim.bo.fileformat:gsub("^unix$", "")
+                        end,
+                    },
+                    "filetype",
+                },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+        })
     end,
 }
