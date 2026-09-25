1a032d49f57fdefaa738eef4a2055a1f238cd8e6
ygdstmidn <121283845+ygdstmidn@users.noreply.github.com>
2026-09-25 14:21:53 +0900

    ✨ カッコをVSCodeっぽい色にする

--- nvim/lua/plugins/rainbow-delimiters.lua
+++ nvim/lua/plugins/rainbow-delimiters.lua
@@ -5,23 +5,14 @@ return {
         local rainbow_delimiters = require("rainbow-delimiters")
 
         vim.g.rainbow_delimiters = {
-            strategy = {
-                [""] = rainbow_delimiters.strategy["global"],
-                vim = rainbow_delimiters.strategy["local"],
-            },
-            query = {
-                [""] = "rainbow-delimiters",
-                lua = "rainbow-blocks",
-            },
             highlight = {
-                "RainbowDelimiterRed",
                 "RainbowDelimiterYellow",
-                "RainbowDelimiterBlue",
-                "RainbowDelimiterOrange",
-                "RainbowDelimiterGreen",
                 "RainbowDelimiterViolet",
                 "RainbowDelimiterCyan",
             },
         }
+        vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#FFD700", force = true })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#DA70D6", force = true })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#179FFF", force = true })
     end,
 }
